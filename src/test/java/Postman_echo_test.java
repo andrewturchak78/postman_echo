0917f231ecf0bbdc2aa1fc7ead21eb865ed40783
Andrey Turchak <andrewturchak78@mail.ru>
2023-03-24 18:58:21 +0300

    test fixed and added

--- src/test/java/Postman_echo_test.java
+++ src/test/java/Postman_echo_test.java
@@ -14,7 +14,7 @@ public class Postman_echo_test {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Privet"))
+                .body("data", equalTo("Hello"))
         ;
     }
 }
