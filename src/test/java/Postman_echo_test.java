92a6d1a241878ea1ed5a8f8b57dfc9cf2e64f4a0
Andrey Turchak <andrewturchak78@mail.ru>
2023-03-24 18:19:37 +0300

    Initial commit

---
+++ src/test/java/Postman_echo_test.java
@@ -0,0 +1,15 @@
+public class Postman_echo_test {
+    // Given - When - Then
+// Предусловия
+    given()
+  .baseUri("https://postman-echo.com")
+  .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+.when()
+  .post("/post")
+// Проверки
+.then()
+  .statusCode(200)
+  .body(/* --> ваша проверка здесь <-- */)
+            ;
+}
